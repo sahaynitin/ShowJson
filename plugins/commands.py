1ec0685d89b893823268d72f5be3b135c2f0b499
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-03 14:46:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,11 +3,6 @@ from config import Config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@Client.on_message(filters.command('start'))
-async def start(c, m):
-    owner = await c.get_users(int(Config.OWNER_ID))
-    owner_username = owner.username if owner.username else 'Tellybots_4u'
-
     # start text
     text = f"""Hey! {m.from_user.mention(style='md')},
 
